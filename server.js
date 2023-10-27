26a3715390ed7aabfbce6412215f069e944e754e
Wlfjd <leejiwon813@naver.com>
2023-10-27 17:55:10 +0900

    mongodb 연결

--- server.js
+++ server.js
@@ -3,11 +3,24 @@ const express = require('express')
 const app=express()
 
 app.use(express.static(__dirname+'public'))
+const { MongoClient } = require('mongodb')
 
-//서버 띄우는 코드 app.listen(포트번호) = 내 컴퓨터 PORT 하나 오픈
+let db
+const url = 'mongodb+srv://wlfjd:wldnjs813!@cluster0.nohbanp.mongodb.net/?retryWrites=true&w=majority'
+new MongoClient(url).connect().then((client)=>{
+  console.log('DB연결성공')
+  //forum 데이터베이스에 연결
+  db = client.db('forum')
+  //자리 이동(성공해야 띄우도록)
+  //서버 띄우는 코드 app.listen(포트번호) = 내 컴퓨터 PORT 하나 오픈
 app.listen(8080,()=>{
     console.log('http://localhost:8080 에서 서버 실행중')
 })
+
+}).catch((err)=>{
+  console.log(err)
+})
+
 
 //서버 기능 - 메인페이지 접속 시 hello를 유저에게 보내기
 app.get('/',(require,response)=>{
@@ -15,7 +28,8 @@ app.get('/',(require,response)=>{
 })
 //news 페이지 만들기
 app.get('/news',(require,response)=>{
-    response.send('sunny day')
+    db.collection('post').insertOne({title:'가나다'})
+    // response.send('sunny day')
 })
 app.get('/shop',(require,response)=>{
     response.send('shopping page')
